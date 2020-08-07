e7c579f79439ed0fa606c1785d1277cb4b5dad62
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-07 11:28:13 +0300

    Initial Commit

---
+++ SettingsScreenUsingFormInSwiftUI/SettingsScreenUsingFormInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsScreenUsingFormInSwiftUIApp.swift
+//  SettingsScreenUsingFormInSwiftUI
+//
+//  Created by ramil on 07.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct SettingsScreenUsingFormInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
